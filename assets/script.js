499889092f523c3e4d0eea6313ad6482ca2eabd7
Antoine Jaunard <antoine.stuff@pm.me>
2022-03-08 18:43:11 +0100

    anim

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+function animateFrom(elem, direction) {
+  direction = direction || 1;
+  var animOffset = 100;
+  var x = 0,
+      y = direction * animOffset;
+  if(elem.classList.contains("gs_reveal_fromLeft")) {
+    x = -animOffset;
+    y = 0;
+  } else if (elem.classList.contains("gs_reveal_fromRight")) {
+    x = animOffset;
+    y = 0;
+  }
+  elem.style.transform = "translate(" + x + "px, " + y + "px)";
+  elem.style.opacity = "0";
+  gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+    duration: 2.5, 
+    x: 0,
+    y: 0, 
+    autoAlpha: 1, 
+    ease: "expo", 
+    overwrite: "auto"
+  });
+}
+
+function hide(elem) {
+  gsap.set(elem, {autoAlpha: 0});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  gsap.registerPlugin(ScrollTrigger);
+  
+  gsap.utils.toArray(".anim").forEach(function(elem) {
+    hide(elem); // assure that the element is hidden when scrolled into view
+    
+    ScrollTrigger.create({
+      trigger: elem,
+      onEnter: function() { animateFrom(elem) }, 
+      onEnterBack: function() { animateFrom(elem, -1) },
+      onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
+    });
+  });
+});
