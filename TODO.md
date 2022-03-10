5091e47704fe7f93ca9f774b94cfb09031531e21
Antoine Jaunard <antoine.stuff@pm.me>
2022-03-10 15:51:58 +0100

    anim changes + positions + images

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO
+
+- [x] Title: text-fabrika_yellow font-titillium font-extralight uppercase text-5xl
+- [x] Title: spacing (see layouts)
+- [/] Vizionář Tomáš Baťa Image: smaller (w-1/2)
+- [/] Big images: 80vh max-height
+- [x] Bigger padding all around the page
+- [ ] animation: 3 differents type (image 1, image 2, text)
--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,18 @@
-function animateFrom(elem, direction) {
+function animateFrom(elem, speed, direction) {
   direction = direction || 1;
-  var animOffset = 100;
   var x = 0,
-      y = direction * animOffset;
+  y = direction * speed/2;
   if(elem.classList.contains("gs_reveal_fromLeft")) {
-    x = -animOffset;
+    x = -speed/2;
     y = 0;
   } else if (elem.classList.contains("gs_reveal_fromRight")) {
-    x = animOffset;
+    x = speed/2;
     y = 0;
   }
   elem.style.transform = "translate(" + x + "px, " + y + "px)";
   elem.style.opacity = "0";
   gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
-    duration: 2.5, 
+    duration: speed/40, 
     x: 0,
     y: 0, 
     autoAlpha: 1, 
@@ -26,17 +25,30 @@ function hide(elem) {
   gsap.set(elem, {autoAlpha: 0});
 }
 
+function scroll(elem, speed) {
+  ScrollTrigger.create({
+    trigger: elem,
+    onEnter: function() { animateFrom(elem, speed) }, 
+    onEnterBack: function() { animateFrom(elem, speed, -1) },
+    onLeave: function() { hide(elem, speed) } // assure that the element is hidden when scrolled into view
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   gsap.registerPlugin(ScrollTrigger);
   
-  gsap.utils.toArray(".anim").forEach(function(elem) {
-    hide(elem); // assure that the element is hidden when scrolled into view
-    
-    ScrollTrigger.create({
-      trigger: elem,
-      onEnter: function() { animateFrom(elem) }, 
-      onEnterBack: function() { animateFrom(elem, -1) },
-      onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
-    });
+  gsap.utils.toArray(".anim-1").forEach(function(elem) {
+    hide(elem);
+    scroll(elem, 100);
+  });
+
+  gsap.utils.toArray(".anim-2").forEach(function(elem) {
+    hide(elem);
+    scroll(elem, 300);
+  });
+
+  gsap.utils.toArray(".anim-3").forEach(function(elem) {
+    hide(elem);
+    scroll(elem, 500);
   });
 });
